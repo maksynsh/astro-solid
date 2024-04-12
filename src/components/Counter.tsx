8c0c5d99434da6876f2fd03e3fe7b7b6f05fce53
Maksym <max.sharinov@gmail.com>
2024-04-12 16:16:42 +0300

    feat: added solid-js simple counter

---
+++ src/components/Counter.tsx
@@ -0,0 +1,17 @@
+import { createSignal, type JSX } from 'solid-js'
+
+export default function Counter() {
+  const [count, setCount] = createSignal(0)
+  const add = () => setCount(count() + 1)
+  const subtract = () => setCount(count() - 1)
+
+  return (
+    <>
+      <div class="flex gap-6 w-full justify-center mt-6">
+        <button class="select-none w-6 h-6" onClick={subtract}>-</button>
+        <pre>{count()}</pre>
+        <button class="select-none w-6 h-6" onClick={add}>+</button>
+      </div>
+    </>
+  )
+}
