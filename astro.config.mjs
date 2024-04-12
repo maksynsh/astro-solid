99e765704f4c35d92bf12e24be7834e95d3c1a12
Maksym <max.sharinov@gmail.com>
2024-04-12 16:27:26 +0300

    feat: added netlify intergation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [tailwind({applyBaseStyles: false}), solidJs()]
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), solidJs()],
+  adapter: netlify()
 });
